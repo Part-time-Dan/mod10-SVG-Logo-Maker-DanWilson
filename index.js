4f1c87349eba5a87d3a7b223a5a1576129bb891d
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-06-25 13:53:22 -0600

    jest install

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const maxLength = require('inquirer-maxlength-input-prompt');
 
 const shapes = require('./lib/shapes');
 
-// prompts to user to collect SVG text, text color, shape and shape color inputs
-const questions = []
+// array of questions to prompt user to collect SVG text, text color, shape and shape color inputs
+inquirer.registerPrompt('maxlen-inp', maxLength);
+const questions = [
+    {
+        type: 'maxlen-inp',
+        name: 'text',
+        message: 'Please enter logo text (3 character limit):',
+        maxLength: 3
+    },
+    {
+        type: 
+    }
+]
