c33ae653d79708907de2b0fa5714808441b3af43
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-06-25 20:23:01 -0600

    generate SVG function

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const maxLength = require('inquirer-maxlength-input-prompt');
 
-const shapes = require('./lib/shapes');
+const generateSVG = require('./lib/shapes');
 
 // array of questions to prompt user to collect SVG text, text color, shape and shape color inputs
-inquirer.registerPrompt('maxlen-inp', maxLength);
+
 const questions = [
     {
         type: 'maxlen-inp',
@@ -14,6 +14,36 @@ const questions = [
         maxLength: 3
     },
     {
-        type: 
+        type: 'input',
+        name: 'textColor',
+        message: 'To choose your text color, enter a valid color keyword or hexadecimal number:'
+    },
+    // {
+    //     type: 'list',
+    //     name: 'shape',
+    //     message: 'What shape would you like your logo to be?',
+    //     choices: [
+    //         "circle",
+    //         "triangel",
+    //         "square"
+    //     ]
+    // },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'To choose your shape color, enter a valid color keyword or hexadecimal number:'
     }
 ]
+
+function init() {
+    inquirer.registerPrompt('maxlen-inp', maxLength);
+    inquirer
+    .prompt(questions)
+    .then((answer) => {
+        return console.log(generateSVG({...answer}))
+
+    }
+    )
+}
+
+init();
