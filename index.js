c814e647ea456bd8a8c7c9dff28a4abad4e79a27
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-06-25 23:09:05 -0600

    pass shape color thru class object

--- index.js
+++ index.js
@@ -7,27 +7,27 @@ const generateSVG = require('./lib/shapes');
 // array of questions to prompt user to collect SVG text, text color, shape and shape color inputs
 
 const questions = [
-    {
-        type: 'maxlen-inp',
-        name: 'text',
-        message: 'Please enter logo text (3 character limit):',
-        maxLength: 3
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'To choose your text color, enter a valid color keyword or hexadecimal number:'
-    },
     // {
-    //     type: 'list',
-    //     name: 'shape',
-    //     message: 'What shape would you like your logo to be?',
-    //     choices: [
-    //         "circle",
-    //         "triangel",
-    //         "square"
-    //     ]
+    //     type: 'maxlen-inp',
+    //     name: 'text',
+    //     message: 'Please enter logo text (3 character limit):',
+    //     maxLength: 3
     // },
+    // {
+    //     type: 'input',
+    //     name: 'textColor',
+    //     message: 'To choose your text color, enter a valid color keyword or hexadecimal number:'
+    // },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like your logo to be?',
+        choices: [
+            "circle",
+            "triangle",
+            "square"
+        ]
+    },
     {
         type: 'input',
         name: 'shapeColor',
