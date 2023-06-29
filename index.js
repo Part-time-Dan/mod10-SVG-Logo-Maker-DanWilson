434ba043fef3d68109cc8f87ad852c5f979ca22c
Part-time-Dan <126934952+Part-time-Dan@users.noreply.github.com>
2023-06-29 02:15:22 -0600

    modularize code

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
 // required packages for functionality
 const inquirer = require('inquirer');
+const questions = require('./lib/questions');
 const fs = require('fs');
 // custom inquirer package will prevent user from entering more than 3 characters
 const maxLength = require('inquirer-maxlength-input-prompt');
 
-const { generateSVG } = require('./lib/shapes');
+const generateSVG = require('./lib/generator');
 
-// array of questions prompting user for SVG specifications
-const questions = [
-    {
-        type: 'maxlen-inp',
-        name: 'text',
-        message: 'Please enter logo text (3 character limit):',
-        maxLength: 3
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'To choose your text color, enter a valid color keyword or hexadecimal number:'
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'What shape would you like your logo to be?',
-        choices: [
-            "circle",
-            "triangle",
-            "square"
-        ]
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'To choose your shape color, enter a valid color keyword or hexadecimal number:'
-    }
-]
-
+//write file and add generated svg code in "examples" folder
 function writeToFile(fileName, data) {
     return fs.writeFile((__dirname + '/examples/' + fileName), data, (err) =>
     err ? console.log(err) : console.log("Generated logo.svg"));
 };
 
+//initiatize program "node index.js"
 function init() {
     inquirer.registerPrompt('maxlen-inp', maxLength);
     inquirer
